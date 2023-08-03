7d5914396cd67c145fe6a708f2db94d02559a5d5
bhupenderthakur7089 <117012030+bhupenderthakur7089@users.noreply.github.com>
2023-08-03 23:41:37 +0530

    create server and send response

--- demo.js
+++ demo.js
@@ -1,12 +1,53 @@
-const product=(a,b)=>a*b;
-console.log(product(4,7));
+// const product=(a,b)=>a*b;
+// console.log(product(4,7));
 
-let student={
-    name:'aryan',
-    age:34,
-    hobbies:['reading','trekking','coding'],
-    showInfo(){
-        console.log(`Name is ${this.name}, age is ${this.age} and hobbies are ${this.hobbies}`)
-    }
+// let student={
+//     name:'aryan',
+//     age:34,
+//     hobbies:['reading','trekking','coding'],
+//     showInfo(){
+//         console.log(`Name is ${this.name}, age is ${this.age} and hobbies are ${this.hobbies}`)
+//     }
+// }
+// student.showInfo();
+
+// let fruits = ['apple', 'oranges', '', 'mango', '', 'lemon'];
+// console.log(fruits.map((fruit) => {
+//     if (fruit == '') {
+//         return 'empty string';
+//     }else return fruit;
+// }))
+
+// const obj1 = {'key1': 1 , 'key2' : 2}
+
+// const obj2 = { ...obj1, key1: 1000}
+
+// console.log(obj1)
+
+// console.log(obj2)
+
+function disp() {
+
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            console.log('c');
+            console.log('d');
+            resolve();
+        }, 2000);
+
+    })
+
 }
-student.showInfo();
+
+async function display() {
+
+    await disp();
+
+}
+
+console.log('a');
+
+console.log('b');
+
+display().then(()=>console.log('e'));
--- firstApp.js
+++ firstApp.js
@@ -1,3 +1,14 @@
-console.log('hello World');
-const fs=require('fs');
-fs.writeFileSync('hello.txt','This is the first text file created using node JS');
+// const fs=require('fs');
+// fs.writeFileSync('hello.txt','This is the first text file created using node JS');
+const http = require('http');
+
+const server = http.createServer(function reqListener(req, res) {
+    // console.log('Bhupender');
+    res.write('<html>');
+    res.write('<head><title>Print Name</title></head>');
+    res.write('<body><h1>Bhupender</h1></body>');
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(4000);
