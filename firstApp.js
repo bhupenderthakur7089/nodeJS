09a54977b39c73eeaadb8fbe42f3919f76e01cba
bhupenderthakur7089 <117012030+bhupenderthakur7089@users.noreply.github.com>
2023-08-04 00:09:41 +0530

    send response for different urls

--- firstApp.js
+++ firstApp.js
@@ -4,11 +4,31 @@ const http = require('http');
 
 const server = http.createServer(function reqListener(req, res) {
     // console.log('Bhupender');
-    res.write('<html>');
-    res.write('<head><title>Print Name</title></head>');
-    res.write('<body><h1>Bhupender</h1></body>');
-    res.write('</html>');
-    res.end();
+    if (req.url == '/') {
+        res.write('<html>');
+        res.write('<head><title>Print Name</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        res.end();
+    } else if (req.url == '/home') {
+        res.write('<html>');
+        res.write('<head><title>Print Name</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        res.end();
+    } else if (req.url == '/about') {
+        res.write('<html>');
+        res.write('<head><title>Print Name</title></head>');
+        res.write('<body><h1>Welcome to about us page</h1></body>');
+        res.write('</html>');
+        res.end();
+    } else if (req.url == '/node') {
+        res.write('<html>');
+        res.write('<head><title>Print Name</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
 });
 
 server.listen(4000);
