d4a7011ad3de8062890aab9e4ab1677860eda782
bhupenderthakur7089 <117012030+bhupenderthakur7089@users.noreply.github.com>
2023-08-08 06:51:40 +0530

    path routing and filtering

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 const express = require('express');
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended:false}));
 
-
-app.use('/add-product', (req, res) => {
-    console.log('In another middleware');
-    res.send('<form action="/product" method="POST"><label for="product">Product Name</label><input type="text" name="product"><label for="size">Product size</label><input type="number" name="size"><button type="submit">Add Product</button></form>')
-});
-app.post('/product', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use('/', (req, res, next) => {
-    console.log('In the middleware');
-    res.send('<h1>This is express framework</h1>')
-});
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/admin', adminRoute);
+app.use('/shop', shopRoute);
+app.use((req, res) => {
+    res.status(404).send('<h1>Page Not found</h1>')
+})
 app.listen(3000);
