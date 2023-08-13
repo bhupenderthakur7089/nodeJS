3299795ac0bfe95ee7132284656228e5c562d0b2
bhupenderthakur7089 <117012030+bhupenderthakur7089@users.noreply.github.com>
2023-08-14 00:11:20 +0530

    views in NodeJS

--- routes/admin.js
+++ routes/admin.js
@@ -8,8 +8,17 @@ router.get('/addProduct', (req, res) => {
 });
 
 router.post('/product', (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
     res.redirect('/shop');
 });
 
+router.get('/contact', (req, res) => {
+    // console.log(req.body);
+    res.sendFile(path.join(__dirname, '../', 'views', 'contactUs.html'));
+});
+
+router.get('/success', (req, res) => {
+    // console.log(req.body);
+    res.sendFile(path.join(__dirname, '../', 'views', 'success.html'));
+});
 module.exports = router;
