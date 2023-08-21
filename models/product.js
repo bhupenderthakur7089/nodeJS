ae8e381a65f680ca0df21e12b0b36bc93f840083
bhupenderthakur7089 <117012030+bhupenderthakur7089@users.noreply.github.com>
2023-08-21 22:21:09 +0530

    writing neat file storage code by reusable function

--- models/product.js
+++ models/product.js
@@ -1,18 +1,24 @@
 const fs = require('fs');
 const pathModule = require('path');
+const path = pathModule.join(__dirname, '../', 'data', 'products.json');
+
+const getFileContent = (cb => {
+    fs.readFile(path, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }
+        return cb(JSON.parse(fileContent));
+    });
+});
 
 module.exports = class Product {
     constructor(name, size) {
         this.productName = name;
         this.productSize = size;
     }
+
     save() {
-        const path = pathModule.join(__dirname, '../', 'data', 'products.json');
-        fs.readFile(path, (err, fileContent) => {
-            let items = [];
-            if (!err) {
-                items = JSON.parse(fileContent);
-            }
+        getFileContent(items => {
             items.push(this);
             fs.writeFile(path, JSON.stringify(items), err => {
                 console.log(err);
@@ -21,12 +27,6 @@ module.exports = class Product {
     }
 
     static fetchAll(cb) {
-        const path = pathModule.join(__dirname, '../', 'data', 'products.json');
-        fs.readFile(path, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+        getFileContent(cb);
     }
 }
